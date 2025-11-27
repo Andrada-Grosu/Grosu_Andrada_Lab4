21622ea0f2be16513ae127600f737e5122c4f307
Andrada <ag.andrada.grosu@gmail.com>
2025-11-27 19:46:43 +0200

    Add project files.

---
+++ Controllers/PredictionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using static Grosu_Andrada_Lab4.PricePredictionModel;
+
+namespace Grosu_Andrada_Lab4.Controllers
+{
+    public class PredictionController : Controller
+    {
+        public IActionResult Price(ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            // Create predection engine related to the loaded train model
+            var modelPath = Path.Combine(Directory.GetCurrentDirectory(), "PricePredictionModel.mlnet");
+
+            ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput,
+           ModelOutput>(mlModel);
+            // Try model on sample data to predict fair price
+            ModelOutput result = predEngine.Predict(input);
+            ViewBag.Price = result.Score;
+            return View(input);
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Grosu_Andrada_Lab4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
