9e38f80c82cc8c24cd9661e57baab899f0550691
Andrada <ag.andrada.grosu@gmail.com>
2025-11-27 20:26:32 +0200

    lab4

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -8,11 +8,10 @@ namespace Grosu_Andrada_Lab4.Controllers
     {
         public IActionResult Price(ModelInput input)
         {
-            // Load the model
+            // === MODEL 1: PRICE ===
             MLContext mlContext = new MLContext();
             // Create predection engine related to the loaded train model
             var modelPath = Path.Combine(Directory.GetCurrentDirectory(), "PricePredictionModel.mlnet");
-
             ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
 
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput,
@@ -20,8 +19,12 @@ namespace Grosu_Andrada_Lab4.Controllers
             // Try model on sample data to predict fair price
             ModelOutput result = predEngine.Predict(input);
             ViewBag.Price = result.Score;
-            return View(input);
+
+
         }
 
+
     }
+
+    
 }
