7ad56ba8b8f98ee5cb2617053017ec4667830a1f
Andrada <ag.andrada.grosu@gmail.com>
2025-12-07 13:57:11 +0200

    lab 5

---
+++ Data/Grosu_Andrada_Lab4Context.cs
@@ -0,0 +1,17 @@
+﻿using Grosu_Andrada_Lab4.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.ML;
+using System.Collections.Generic;
+
+namespace Grosu_Andrada_Lab4.Data
+{
+    public class Grosu_Andrada_Lab4Context : DbContext
+    {
+        public Grosu_Andrada_Lab4Context(DbContextOptions<Grosu_Andrada_Lab4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PredictionHistory> PredictionHistory { get; set; } = default!;
+    }
+}
---
+++ Models/PredictionHistory.cs
@@ -0,0 +1,10 @@
+﻿namespace Grosu_Andrada_Lab4.Models
+{
+    public class PredictionHistory
+    {
+        public int Id { get; set; }
+        public float PredictedPrice { get; set; }
+        public string? InputData { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
